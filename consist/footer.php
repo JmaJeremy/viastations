37f4a4123aae75a08a9afc2d36f1a9810ab16f11
Jeremy Andrews <thejeremy.net@gmail.com>
2021-01-09 01:42:41 +0000

    updating footer

--- consist/footer.php
+++ consist/footer.php
@@ -1,3 +1,4 @@
-<div class="footer">This is an independently operated website, and is not associated with nor endorsed by Via Rail Canada. Copyright &copy;2020 Jeremy Andrews.<br />
+<div class="footer">This is an independently operated website, and is not associated with nor endorsed by Via Rail Canada. "VIA Rail" is a trademark of Via Rail Canada Inc.<br />
+		This website Copyright &copy;<?php echo date("Y"); ?> Jeremy Andrews.<br />
 		For technical support or other inquiries, contact <a href="mailto:webmaster@viarail.live">webmaster@viarail.live</a><br />
 		The source code for this website is freely available under the MIT license on <a target="_blank" href="https://github.com/JmaJeremy/viastations">Github</a>. </div>
